d12f637ce9598d1565ca79bb1946b9fd6e29ef87
rynartisan <rynartisan@users.noreply.github.com>
2025-06-01 12:15:37 +0000

    deploy: d487b20cc9ab0a030675bd0219df555cf7653b23

---
+++ _app/immutable/chunks/CpZgyZFj.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1c8xz81)==null?void 0:s.base)??"/rynartisan";var a;const e=((a=globalThis.__sveltekit_1c8xz81)==null?void 0:a.assets)??t;export{e as a,t as b};
